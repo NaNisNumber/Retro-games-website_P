20c80b34680b480e9a64599a785c77a4432d157d
Tobirama <grigoroscuta.sergiu@yahoo.com>
2023-04-27 19:52:32 +0300

    bugfix: currentSliderIndex is not updating properly when the slider goes auto, and then the user goes to a prev slide(the currentSliderIndex is still 0)

--- vite-project/src/components/feature-section/FeatureSection.jsx
+++ vite-project/src/components/feature-section/FeatureSection.jsx
@@ -1,5 +1,4 @@
 import { React, Fragment, useState, useRef } from "react";
-import { nanoid } from "nanoid";
 import "./FeatureSection.css";
 import { useEffect } from "react";
 
@@ -7,6 +6,9 @@ const FeatureSection = ({ mainMenuIsClosed }) => {
   const [sliderImgsExist, setSliderImgsExist] = useState(false);
   const sliderRef = useRef(null);
   const [games, setSliderGames] = useState([]);
+  const [currentSliderGameId, setCurrentSliderGameId] = useState([]);
+  const [sliderDirection, setSliderDirection] = useState(0);
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
 
   useEffect(() => {
     const retrieveGameData = async () => {
@@ -21,9 +23,9 @@ const FeatureSection = ({ mainMenuIsClosed }) => {
     if (games.length === 0) return;
     const sliderImgs = games.map((game, i) => {
       const gameUrl = game.imgUrl;
-
+      const gameId = game.id;
       return (
-        <figure className="gaming__feature-img-container">
+        <figure data-gameid={gameId} className="gaming__feature-img-container">
           <img
             alt="game"
             src={gameUrl}
@@ -36,26 +38,92 @@ const FeatureSection = ({ mainMenuIsClosed }) => {
     setSliderImgsExist(true);
   }
 
-  const sliderSettings = {
-    direction: 0,
-    currentSlideIndex: 0,
-  };
+  // const sliderSettings = {
+  //   direction: 0,
+  //   currentSlideIndex: 0,
+  // };
+
+  // useEffect(() => {
+  //   const slides = document.querySelectorAll(".gaming__feature-img-container");
+
+  //   if (!sliderRef) return;
+  //   const lastSlideIndex = slides.length - 1;
+
+  //   let intervalId = setInterval(() => {
+  //     nextSlide(lastSlideIndex, slides);
+  //   }, 2500);
+  //   sliderRef.current.addEventListener("mouseenter", () => {
+  //     clearInterval(intervalId);
+  //   });
+  //   sliderRef.current.addEventListener("mouseleave", () => {
+  //     intervalId = setInterval(() => {
+  //       return nextSlide(lastSlideIndex, slides);
+  //     }, 2500);
+  //   });
+
+  //   const changeSlidesPosition = () => {
+  //     const slideWidth = slides[0].getBoundingClientRect().width;
+  //     for (let i = 0; i < slides.length; i++) {
+  //       slides[i].style.left = `${slideWidth * i}px`;
+  //     }
+  //   };
+  //   if (sliderImgsExist) {
+  //     changeSlidesPosition();
+  //   }
+  // }, [sliderImgsExist, mainMenuIsClosed]);
+
+  // function nextSlide(lastSlideIndex, slides) {
+  //   sliderSettings.direction -= 100;
+  //   sliderSettings.currentSlideIndex++;
+
+  //   if (sliderSettings.currentSlideIndex > lastSlideIndex) {
+  //     sliderSettings.currentSlideIndex = 0;
+  //     sliderSettings.direction = 0;
+  //   }
+
+  //   slides.forEach((slide) => {
+  //     slide.style.transform = `translateX(${sliderSettings.direction}%)`;
+  //   });
+  // }
+
+  // function prevSlide(lastSlideIndex, slides) {
+  //   if (sliderSettings.currentSlideIndex === 0) {
+  //     sliderSettings.currentSlideIndex = lastSlideIndex;
+  //     sliderSettings.direction = -`${lastSlideIndex * 100}`;
+  //   } else if (sliderSettings.currentSlideIndex > 0) {
+  //     sliderSettings.currentSlideIndex--;
+  //     sliderSettings.direction += 100;
+  //   }
+  //   slides.forEach((slide) => {
+  //     slide.style.transform = `translateX(${sliderSettings.direction}%)`;
+  //   });
+  // }
+
+  // const moveSlides = (e) => {
+  //   const slides = document.querySelectorAll(".gaming__feature-img-container");
+  //   const lastSlideIndex = slides.length - 1;
+
+  //   if (e.target.parentElement.id === "gaming__feature-move-slider-prev") {
+  //     prevSlide(lastSlideIndex, slides);
+  //   } else nextSlide(lastSlideIndex, slides);
+  //   console.log(sliderSettings.currentSlideIndex);
+  // };
 
   useEffect(() => {
     const slides = document.querySelectorAll(".gaming__feature-img-container");
 
-    if (!sliderRef) return;
-    const lastSlideIndex = slides.length - 1;
+    if (!sliderRef || slides.length - 1 < 0) return;
 
     let intervalId = setInterval(() => {
-      return nextSlide(lastSlideIndex, slides);
+      return nextSlide();
     }, 2500);
+
     sliderRef.current.addEventListener("mouseenter", () => {
       clearInterval(intervalId);
     });
     sliderRef.current.addEventListener("mouseleave", () => {
       intervalId = setInterval(() => {
-        return nextSlide(lastSlideIndex, slides);
+        return nextSlide();
       }, 2500);
     });
 
@@ -70,40 +138,40 @@ const FeatureSection = ({ mainMenuIsClosed }) => {
     }
   }, [sliderImgsExist, mainMenuIsClosed]);
 
-  function nextSlide(lastSlideIndex, slides) {
-    sliderSettings.direction -= 100;
-    sliderSettings.currentSlideIndex++;
-
-    if (sliderSettings.currentSlideIndex > lastSlideIndex) {
-      sliderSettings.currentSlideIndex = 0;
-      sliderSettings.direction = 0;
-    }
-
-    slides.forEach((slide) => {
-      slide.style.transform = `translateX(${sliderSettings.direction}%)`;
-    });
+  function nextSlide() {
+    setSliderDirection((prevSliderDirection) => prevSliderDirection - 100);
+    setCurrentSlideIndex((prevSliderIndex) => prevSliderIndex + 1);
   }
 
-  function prevSlide(lastSlideIndex, slides) {
-    if (sliderSettings.currentSlideIndex === 0) {
-      sliderSettings.currentSlideIndex = lastSlideIndex;
-      sliderSettings.direction = -`${lastSlideIndex * 100}`;
-    } else if (sliderSettings.currentSlideIndex > 0) {
-      sliderSettings.currentSlideIndex--;
-      sliderSettings.direction += 100;
+  function prevSlide(lastSlideIndex) {
+    if (currentSlideIndex === 0) {
+      setCurrentSlideIndex(lastSlideIndex);
+      setSliderDirection(-`${lastSlideIndex * 100}`);
+    } else if (currentSlideIndex > 0) {
+      setCurrentSlideIndex((prevSlideIndex) => prevSlideIndex - 1);
+      setSliderDirection((prevSliderDirection) => prevSliderDirection + 100);
     }
-    slides.forEach((slide) => {
-      slide.style.transform = `translateX(${sliderSettings.direction}%)`;
-    });
   }
+  useEffect(() => {
+    const slides = document.querySelectorAll(".gaming__feature-img-container");
+    const lastSlideIndex = slides.length - 1;
+    if (currentSlideIndex > lastSlideIndex) {
+      setSliderDirection(0);
+      setCurrentSlideIndex(0);
+    }
+    // move slides
+    slides.forEach((slide) => {
+      slide.style.transform = `translateX(${sliderDirection}%)`;
+    });
+  }, [sliderDirection]);
 
   const moveSlides = (e) => {
     const slides = document.querySelectorAll(".gaming__feature-img-container");
     const lastSlideIndex = slides.length - 1;
 
     if (e.target.parentElement.id === "gaming__feature-move-slider-prev") {
-      prevSlide(lastSlideIndex, slides);
-    } else nextSlide(lastSlideIndex, slides);
+      prevSlide(lastSlideIndex);
+    } else nextSlide();
   };
 
   return (
