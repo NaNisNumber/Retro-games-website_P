b3d941dbe1d10d219969e3d429a1ca737ae23a0c
Tobirama <grigoroscuta.sergiu@yahoo.com>
2023-05-14 18:54:51 +0300

    bugfix: adding a new dependency besides slidesRef.current, so that the slider starts again when the user leave the page and come back to it later

--- vite-project/src/components/feature-section/FeatureSection.jsx
+++ vite-project/src/components/feature-section/FeatureSection.jsx
@@ -1,16 +1,27 @@
 import { React, Fragment, useState, useRef } from "react";
 import "./FeatureSection.css";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const FeatureSection = ({ mainMenuIsClosed }) => {
-  const [sliderImgsExist, setSliderImgsExist] = useState(false);
   const sliderRef = useRef(null);
+  const slidesContainer = useRef(null);
   const [games, setSliderGames] = useState([]);
-  const [currentSliderGameId, setCurrentSliderGameId] = useState([]);
+  const [currentSliderGameId, setCurrentSliderGameId] = useState(null);
   const [sliderDirection, setSliderDirection] = useState(0);
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  const slidesRef = useRef(null);
+
+  const getSlidesMap = () => {
+    if (!slidesRef.current) {
+      slidesRef.current = new Map();
+    }
+
+    return slidesRef.current;
+  };
 
   useEffect(() => {
+    // games for slider
     const retrieveGameData = async () => {
       const response = await fetch("http://localhost:5001/");
       const games = await response.json();
@@ -19,13 +30,24 @@ const FeatureSection = ({ mainMenuIsClosed }) => {
     retrieveGameData();
   }, []);
 
-  function createSliders() {
+  function createSlides() {
     if (games.length === 0) return;
     const sliderImgs = games.map((game, i) => {
       const gameUrl = game.imgUrl;
       const gameId = game.id;
       return (
-        <figure data-gameid={gameId} className="gaming__feature-img-container">
+        <figure
+          key={gameId}
+          ref={(slide) => {
+            const map = getSlidesMap();
+
+            if (slide) {
+              map.set(i, slide);
+            }
+          }}
+          data-gameid={gameId}
+          className="gaming__feature-img-container"
+        >
           <img
             alt="game"
             src={gameUrl}
@@ -34,86 +56,39 @@ const FeatureSection = ({ mainMenuIsClosed }) => {
         </figure>
       );
     });
-    if (sliderImgsExist) return sliderImgs;
-    setSliderImgsExist(true);
+    return sliderImgs;
   }
-
-  // const sliderSettings = {
-  //   direction: 0,
-  //   currentSlideIndex: 0,
-  // };
-
-  // useEffect(() => {
-  //   const slides = document.querySelectorAll(".gaming__feature-img-container");
-
-  //   if (!sliderRef) return;
-  //   const lastSlideIndex = slides.length - 1;
-
-  //   let intervalId = setInterval(() => {
-  //     nextSlide(lastSlideIndex, slides);
-  //   }, 2500);
-  //   sliderRef.current.addEventListener("mouseenter", () => {
-  //     clearInterval(intervalId);
-  //   });
-  //   sliderRef.current.addEventListener("mouseleave", () => {
-  //     intervalId = setInterval(() => {
-  //       return nextSlide(lastSlideIndex, slides);
-  //     }, 2500);
-  //   });
-
-  //   const changeSlidesPosition = () => {
-  //     const slideWidth = slides[0].getBoundingClientRect().width;
-  //     for (let i = 0; i < slides.length; i++) {
-  //       slides[i].style.left = `${slideWidth * i}px`;
-  //     }
-  //   };
-  //   if (sliderImgsExist) {
-  //     changeSlidesPosition();
-  //   }
-  // }, [sliderImgsExist, mainMenuIsClosed]);
-
-  // function nextSlide(lastSlideIndex, slides) {
-  //   sliderSettings.direction -= 100;
-  //   sliderSettings.currentSlideIndex++;
-
-  //   if (sliderSettings.currentSlideIndex > lastSlideIndex) {
-  //     sliderSettings.currentSlideIndex = 0;
-  //     sliderSettings.direction = 0;
-  //   }
-
-  //   slides.forEach((slide) => {
-  //     slide.style.transform = `translateX(${sliderSettings.direction}%)`;
-  //   });
-  // }
-
-  // function prevSlide(lastSlideIndex, slides) {
-  //   if (sliderSettings.currentSlideIndex === 0) {
-  //     sliderSettings.currentSlideIndex = lastSlideIndex;
-  //     sliderSettings.direction = -`${lastSlideIndex * 100}`;
-  //   } else if (sliderSettings.currentSlideIndex > 0) {
-  //     sliderSettings.currentSlideIndex--;
-  //     sliderSettings.direction += 100;
-  //   }
-  //   slides.forEach((slide) => {
-  //     slide.style.transform = `translateX(${sliderSettings.direction}%)`;
-  //   });
-  // }
-
-  // const moveSlides = (e) => {
-  //   const slides = document.querySelectorAll(".gaming__feature-img-container");
-  //   const lastSlideIndex = slides.length - 1;
-
-  //   if (e.target.parentElement.id === "gaming__feature-move-slider-prev") {
-  //     prevSlide(lastSlideIndex, slides);
-  //   } else nextSlide(lastSlideIndex, slides);
-  //   console.log(sliderSettings.currentSlideIndex);
-  // };
+  const slides = createSlides();
 
   useEffect(() => {
-    const slides = document.querySelectorAll(".gaming__feature-img-container");
+    if (!slidesRef.current) return;
+    const currentSlide = slidesRef.current.get(currentSlideIndex);
+    if (currentSlide) {
+      const currentGameId = currentSlide.dataset.gameid;
+      setCurrentSliderGameId(currentGameId);
+    }
+  }, [slidesRef.current, games, currentSlideIndex]);
 
-    if (!sliderRef || slides.length - 1 < 0) return;
+  useEffect(() => {
+    if (!slidesRef.current || !slidesRef.current.size) return;
+    const changeSlidesPosition = () => {
+      const slideWidth = slidesRef.current.get(0).getBoundingClientRect().width;
+      for (let i = 0; i < slidesRef.current.size; i++) {
+        slidesRef.current.get(i).style.left = `${slideWidth * i}px`;
+      }
+    };
 
+    changeSlidesPosition();
+  }, [slidesRef.current, games]);
+
+  useEffect(() => {
+    if (!slidesRef.current) return;
+    if (
+      !sliderRef ||
+      slidesRef.current.size - 1 < 0 ||
+      currentSlideIndex > slidesRef.current.size - 1
+    )
+      return;
     let intervalId = setInterval(() => {
       return nextSlide();
     }, 2500);
@@ -121,22 +96,13 @@ const FeatureSection = ({ mainMenuIsClosed }) => {
     sliderRef.current.addEventListener("mouseenter", () => {
       clearInterval(intervalId);
     });
+
     sliderRef.current.addEventListener("mouseleave", () => {
       intervalId = setInterval(() => {
         return nextSlide();
       }, 2500);
     });
-
-    const changeSlidesPosition = () => {
-      const slideWidth = slides[0].getBoundingClientRect().width;
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.left = `${slideWidth * i}px`;
-      }
-    };
-    if (sliderImgsExist) {
-      changeSlidesPosition();
-    }
-  }, [sliderImgsExist, mainMenuIsClosed]);
+  }, [slidesRef.current, mainMenuIsClosed]);
 
   function nextSlide() {
     setSliderDirection((prevSliderDirection) => prevSliderDirection - 100);
@@ -152,22 +118,24 @@ const FeatureSection = ({ mainMenuIsClosed }) => {
       setSliderDirection((prevSliderDirection) => prevSliderDirection + 100);
     }
   }
+
   useEffect(() => {
-    const slides = document.querySelectorAll(".gaming__feature-img-container");
-    const lastSlideIndex = slides.length - 1;
+    if (!slidesRef.current) return;
+    const lastSlideIndex = slidesRef.current.size - 1;
+
     if (currentSlideIndex > lastSlideIndex) {
       setSliderDirection(0);
       setCurrentSlideIndex(0);
     }
     // move slides
-    slides.forEach((slide) => {
+
+    slidesRef.current.forEach((slide) => {
       slide.style.transform = `translateX(${sliderDirection}%)`;
     });
-  }, [sliderDirection]);
+  }, [slidesRef.current, currentSlideIndex]);
 
   const moveSlides = (e) => {
-    const slides = document.querySelectorAll(".gaming__feature-img-container");
-    const lastSlideIndex = slides.length - 1;
+    const lastSlideIndex = slidesRef.current.size - 1;
 
     if (e.target.parentElement.id === "gaming__feature-move-slider-prev") {
       prevSlide(lastSlideIndex);
@@ -229,11 +197,18 @@ const FeatureSection = ({ mainMenuIsClosed }) => {
               <ion-icon name="arrow-forward"></ion-icon>
             </button>
             <div className="gaming__feature-game-cta-container">
-              <button className="gaming__feature-game-page-btn">
-                See Game
-              </button>
+              <Link to={`shop/${currentSliderGameId}`}>
+                <button className="gaming__feature-game-page-btn">
+                  See Game
+                </button>
+              </Link>
             </div>
-            {createSliders()}
+            <div
+              ref={slidesContainer}
+              className="gaming__feature-slides-container"
+            >
+              {slides}
+            </div>
           </div>
         </div>
       </section>
